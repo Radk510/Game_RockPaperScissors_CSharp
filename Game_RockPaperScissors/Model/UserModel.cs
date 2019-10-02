9dd7b0f652830bd375169abc588e99a2b414f40e
radk510 <radk510@yandex.ru>
2019-10-02 14:33:00 +0300

    create user class and it's description

---
+++ Game_RockPaperScissors/Model/UserModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game_RockPaperScissors
+{
+    class UserModel
+    {
+        public string ChoseWhatToUse()
+        {
+            Console.WriteLine("Chose what you want to use: Rock (press 'r'), Paper (press 'p') or Scissors (press 's')");
+            Console.Write("Write your choise here: ");
+            string user_choise = Console.ReadLine().ToLower();
+            switch (user_choise)
+            {
+                case "r":
+                    return "Rock";
+                case "p":
+                    return "Paper";
+                case "s":
+                    return "Scissors";
+                default:
+                    return "Wrong choise";
+            }
+        }
+    }
+}
---
+++ Game_RockPaperScissors/Structure/model_description.md
@@ -0,0 +1,10 @@
+﻿## UserModel
+
+### Methods
+
+1. ChoseWhatToUse() - this method gets an apportunity to user to chose what to use: Rock, Paper or Scissors
+
+1.1. **How it's works:** 
+- In this method we print to console a question: Chose what you want to use: Rock (press 'r'), Paper (press 'p') or Scissors (press 's')
+- Method gets user an ability to enter his choise by using Console.ReadLine().ToLower() method.
+- Method returns the string with name of choise.
