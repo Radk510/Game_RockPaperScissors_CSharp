8bde9e6f5b2fc8b5494808af5e55172618ab6f7a
radk510 <radk510@yandex.ru>
2019-10-02 15:51:10 +0300

    start to create view model

---
+++ Game_RockPaperScissors/Specification/view_description.md
@@ -0,0 +1,11 @@
+﻿## class:  GameView (View)
+
+### Methods
+
+1. *GameTitles()* - here we declare the game title and show current date: **Console Game: \"Rock, Paper, Scissors\ + GetCurrentDate()**
+
+2. *GetCurrentDate()* - in this method we get current date in default form
+
+//3. *ProposalToChoseInstrument()* - returns to user a string with proposal to chose what to use: **Chose what you want to use: Rock (press 'r'), Paper (press 'p') or Scissors (press 's')**
+
+4. 
---
+++ Game_RockPaperScissors/View/GameView.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game_RockPaperScissors.View
+{
+    class GameView
+    {
+        private DateTime GetCurrentDate()
+        {
+            DateTime date = DateTime.Now;
+            return date;
+        }
+        public string GameTitles()
+        {
+            string title = "Console Game: \"Rock, Paper, Scissors\" " + GetCurrentDate();
+            return title;
+        }
+
+        
+    }
+}
